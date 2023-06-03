d4b5a3d81631f6fe0e6ff9769dc449424657d9df
Atul Yadav <106364254+ATULYADAVpro@users.noreply.github.com>
2023-06-03 18:32:28 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const APIURL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI =
+    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+// ******************************************
+/* <img src="${imagePath}" alt="" />
+<div class="overlay">
+    <div class="title"> 
+        <h2> ${result.original_title}  </h2>
+        <span> ${result.vote_average} <span>
+    </div>
+    <h3>Overview:</h3>
+    <p> 
+        ${result.overview}
+    </p>
+ </div> */
